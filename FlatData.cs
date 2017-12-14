9426d431da3376ec2e08cd943648829a08445afc
bhavya1718 <34507672+bhavya1718@users.noreply.github.com>
2017-12-14 16:36:54 +0530

    Add files via upload

---
+++ FlatData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RWA1
+{
+    public class FlatData
+    {
+        public int Flat_no { get; set; }
+        public int Floor { get; set; }
+        public string User_id { get; set; }
+        public int Resident_count { get; set; }
+        public string Date_of_own { get; set; }
+        public string Flat_type { get; set; }
+        public string Parking_space { get; set; }
+
+        public static implicit operator FlatData(List<FlatData> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RWA1
+{
+    public class UserData
+    {
+        public string User_id { get; set; }
+        public string User_name { get; set; }
+        public long User_number { get; set; }
+        public List<FlatData> Flats { get; set; }
+    }
+}
